624f79ff460de409b7bb877f396f69d80f51457d
Chelsi Jain <jainche1220@gmail.com>
2026-06-10 17:22:56 +0000

    datatype and comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+/*console.log (2>3)
+console.log (2>=3)
+console.log (2==3)
+console.log (2!=3)
+console.log (2<3)
+console.log (2<=3)*/
+
+/*console.log ("2"<3)
+console.log ("02"==2)
+console.log ("2"<=3)*/
+
+/*console.log (null<0)//false
+console.log (null>0)//false
+console.log (null==0)//false
+console.log (null>=0)
+//=> true because in javascript when we use comaparison operation <,>,<=,>=
+//it is treated differently than ==*/
+
+console.log (undefined<0)//false
+console.log (undefined>0)//false
+console.log (undefined==0)//false
+console.log (undefined>=0)//false
+
+//=== this is struct check this donot convert the datatype and compare the datatype also
+
+console.log("2" === 2);
