15a1afc40b1b71f95dfe54673998b3c68bdf8827
Chelsi Jain <jainche1220@gmail.com>
2026-06-10 09:29:07 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+let name ="chelsi jain"
+let age = 22
+/* data types 
+string = ""
+number => 2 to power 53 thk range hoti hai 
+bigint
+booleen = true/false
+null => standalone value 
+undefined => 
+symbol => unique (jo hm jyada use react me krege )*/
+
+console.log(typeof null) //(jb hm iska data type nikalte hai toh object aata hai)
+console.log(typeof undefined) //jb hm iska data type nikalte hai toh undefined hi aa ta hai 
