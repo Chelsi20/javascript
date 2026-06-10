9d038dd6d654db5f100bc24f08ec8eafdbb8ba46
Chelsi Jain <jainche1220@gmail.com>
2026-06-10 08:48:12 +0000

    data_type

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "chelsi12@gmail.com"
+var accountPsswd = "1234"
+accountCity = "Gurugram"
+let accountState;
+
+//accountId = 2
+accountEmail = "chelsi@gmail.com"
+accountPsswd = "5678"
+accountCity = "delhi"
+
+
+console.log(accountId);
+console.table([accountEmail,accountPsswd,accountId,accountCity,accountState])
