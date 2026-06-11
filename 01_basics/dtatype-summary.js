a44bf031d2b4c54bd2d53ac7c462422f2e69d8ae
Chelsi Jain <jainche1220@gmail.com>
2026-06-11 18:35:07 +0000

    strings

--- 01_basics/dtatype-summary.js
+++ 01_basics/dtatype-summary.js
@@ -35,3 +35,35 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack AND Heap
+/* Stack (primitive) (call by number)==> koi bhi variable ki jo value hoti hai vo copy hoti
+ na na ki original value vhange hoti hai
+ For example:*/
+
+ let gameName = "chelsi jain"
+ let anothername = gameName 
+ anothername = "kavish jain"
+ console.table([gameName, anothername]);
+ console.log(gameName)
+ console.log(anothername)
+
+ /*Heap (Reference type) => koi bhi variable ki value ek heap/space me store hoti hai or waha
+ se uska reference milta hai. agar hum us reference ko kisi dusre variable me assign karte hai 
+ to dono variable same reference ko point karte hai.
+    For example:*/
+
+ let userOne = {
+    email: "chelsijain.com",
+    upi: "chelsi@ybl"
+ }
+
+ let userTwo = userOne
+
+userTwo.email = "jaichelsi@87.com";
+console.log (userOne.email);
+console.log (userTwo.email);
+
---
+++ 01_basics/string.js
@@ -0,0 +1,7 @@
+let name = "chelsi jain"
+let repoCount = 50
+console.log(name+repoCount+ " Vamlue ")
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`)// String Interpolation
+const gameName = new String('chelsi-jain-123')
+console.log(gameName[2])
